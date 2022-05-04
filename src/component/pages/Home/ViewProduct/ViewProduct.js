2d49f8d5abe852ac0cc06850f98c1f66b5d20c11
masud1989 <mranasrj@gmail.com>
2022-05-04 10:13:12 +0600

    product-display-on-inventory-page

--- src/component/pages/Home/ViewProduct/ViewProduct.js
+++ src/component/pages/Home/ViewProduct/ViewProduct.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
 import img from './../../../../images/slider/slider-1.jpg';
 
 const ViewProduct = () => {
@@ -12,32 +13,35 @@ const ViewProduct = () => {
 
     return (
         <div className='container mb-5  w-75'>
-            <h4 className='text-info mb-3 bg-info text-white p-2'>Product List</h4>
+            <h4 className='text-info mb-3 bg-info text-white p-2'>Product List:{products.length}</h4>
             <table className="table table-sm mb-10">
-            <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Supplier</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                
-                <tr>
-                    <th scope="row">Name</th>
-                    <td><img style={{height: "70px", width: "90px"}} src={img} alt='' /></td>
-                    <td>MS Enterprise</td>
-                    <td>250000</td>
-                    <td>4</td>
-                    <td>
-                        <a className='btn btn-info mx-1'>Edit</a>
-                        <a className='btn btn-danger mx-1'>Delete</a>
-                    </td>
-                </tr>
-            </tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Supplier</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(product=>
+                            <tr>
+                                <td scope="row">Name{product.name}</td>
+                                <td><img style={{height: "70px", width: "90px"}} src={product.img} alt='' /></td>
+                                <td>{product.supplier}</td>
+                                <td>{product.price}</td>
+                                <td>{product.quantity}</td>
+                                <td>
+                                    <a className='btn btn-info mx-1'>Edit</a>
+                                    <a className='btn btn-danger mx-1'>Delete</a>
+                                </td>                                
+                            </tr>
+                            )
+                    }
+                </tbody>
             </table>
         </div>
     );
